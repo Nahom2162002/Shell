aff46a9062f442cb862c5c93508863b4cc529605
Nahom2162002 <NahomAshagrea2002@gmail.com>
2026-03-12 23:52:51 -0400

    Header functions initialized

---
+++ shell.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "shell.h"
+
+int main(char** args) {
+
+}
+
+void builtin_cd(char** args) {
+
+}
+
+void builtin_pwd() {
+
+}
+
+void builtin_exit(char** args) {
+
+}
+
+void cat(char** args) {
+
+}
+
+void echo(char** args) {
+
+}
+
+void ls(char** args) {
+    
+}
