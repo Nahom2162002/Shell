9004f18b9173815295e30f7c71765dbcc768d79c
Nahom2162002 <NahomAshagrea2002@gmail.com>
2026-03-13 12:54:11 -0400

    Updated header file

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_COMMANDS
+#define SHELL_COMMANDS
+
+void builtin_cd(char** args);
+
+void builtin_pwd();
+
+void builtin_exit(char** args);
+
+void cat(char** args);
+
+void echo(char** args);
+
+void ls(char** args);
+
+#endif 
