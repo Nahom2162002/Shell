53203e6d884c2512d49e045d4b091fc73dbfbc22
Nahom2162002 <NahomAshagrea2002@gmail.com>
2026-03-29 00:00:41 -0400

    Implemented cat function + fixed argument error in main function

--- shell.c
+++ shell.c
@@ -3,12 +3,14 @@
 #include <unistd.h>
 #include "shell.h"
 
-int main(char** args) {
+#define MAX_BUFFER 256
+
+int main(int argc, char** args) {
     printf("Welcome to this custom based UNIX Shell that I made! This is just the beginning so as time passes there will certainly be updates.\n");
     printf("~$");
-    char * command;
+    char command[MAX_BUFFER];
 
-    while (fgets(command, sizeof(command), stdin)) {
+    while (fgets(command, MAX_BUFFER, stdin)) {
 
     }
 }
@@ -47,7 +49,12 @@ void builtin_exit(char** args) {
 }
 
 void cat(char** args) {
+    FILE* content = fopen(args[1], "r");
+    char line[MAX_BUFFER];
 
+    while (fgets(line, MAX_BUFFER, content) != NULL) {
+        printf("%s\n", line);
+    }
 }
 
 void echo(char** args) {
@@ -56,7 +63,7 @@ void echo(char** args) {
 
 void ls(char** args) {
     pid_t pid;
-    char* arguments[2] = { "ls", NULL };
+    //char* arguments[2] = { "ls", NULL };
 
     pid = fork();
 
@@ -65,7 +72,7 @@ void ls(char** args) {
         exit(EXIT_FAILURE);
     }
     else if (pid == 0) {
-        execvp(arguments[0], arguments);
+        execvp(args[0], args);
         exit(EXIT_SUCCESS);
     }
 }
